c11d64a462bca3c7020a33eb07940af4cdf27203
Tralamy <50752445+Tralamy442@users.noreply.github.com>
2021-11-07 17:55:09 -0500

    ÃMarkdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeSanitize from 'rehype-sanitize';
+import remarkToc from "remark-toc";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+// import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+// import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import 'katex/dist/katex.min.css' // `rehype-katex` does not import the CSS for you
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [text, setText] = useState("");
+
+    const onChange = (e: any) => {
+        // @ts-ignore
+        setText(document.getElementById("input").value);
+    }
+    return (
+        <div className="App flex justify-center">
+            <textarea onChange={onChange} name="input" id="input" className="bg-blue-200 w-full h-full"/>
+            <div id="output" className="bg-gradient-to-r from-red-400 to-red-500 w-full justify-start text-left">
+                <ReactMarkdown
+                    children={text}
+                    remarkPlugins={[remarkMath, remarkGfm, remarkToc]}
+                    rehypePlugins={[rehypeKatex, [rehypeHighlight, {ignoreMissing: true}], rehypeRaw]}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
